59db0d4dddc4a5c37b5fd5d4b4caf421d0c50b2f
khodosko <dkhodosko@gmail.com>
2022-03-18 17:33:11 -0700

    moved tags

--- orthanc-pacs/modify.lua
+++ orthanc-pacs/modify.lua
@@ -21,7 +21,45 @@ function OnStoredInstance(instanceId, tags, metadata, origin)
       -- Station Name
       modifyRequest["Replace"]["StationName"] = "noName"
 
-      -- testing tags 
+
+      modifyRequest["Force"] = true  -- because we want to keep the same SOPInstanceUID
+
+      -- download a modified version of the instance
+      local modifiedDicom = RestApiPost('/instances/' .. instanceId .. '/modify', DumpJson(modifyRequest))
+
+      -- upload the modified instance.  When performing modification at the instance level, all IDs from the original instance
+      -- are preserved (SOPInstanceUID, SeriesInstanceUID, StudyInstanceUID)
+      -- so when you'll upload it to Orthanc, it will overwrite the old instance only
+      -- if you've set the "OverwriteInstances" option to true in your configuration file
+      local uploadResponse = ParseJson(RestApiPost('/instances', modifiedDicom))
+
+      -- PrintRecursive(uploadResponse)
+      
+      if (uploadResponse["Status"] == 'AlreadyStored') then
+         print("Are you sure you've enabled 'OverwriteInstances' option ?")
+      end
+
+      if (uploadResponse["ID"] ~= instanceId) then
+         print("modified instance and original instance don't have the same Orthanc IDs !")
+      end 
+
+      print('replaced InstitutionName in instance ' .. instanceId)
+
+   end
+end
+
+function OnStableStudy(studyId, tags, metadata)
+   -- since we have modified tags that are stored in the Index DB at Study/Series level: InstitutionName and OperatorsName,
+   -- we need to reconstruct the Index DB data for this study
+
+   RestApiPost('/studies/' .. studyId .. '/reconstruct', "")
+
+   print('reconstructed Index DB data for study ' .. studyId)
+
+end
+
+
+-- testing tags 
 
       -- modifyRequest["Replace"]["PatientID"] = "000000000000"
       -- modifyRequest["Replace"]["StudyDate"] = "0"
@@ -129,40 +167,3 @@ function OnStoredInstance(instanceId, tags, metadata, origin)
       --  modifyRequest["Replace"]["0010,0040"] = "1"
       --  --Related Frame of Reference UID	(3006,00C2)
       --  modifyRequest["Replace"]["3006,00C2"] = "1"
-
-
-      modifyRequest["Force"] = true  -- because we want to keep the same SOPInstanceUID
-
-      -- download a modified version of the instance
-      local modifiedDicom = RestApiPost('/instances/' .. instanceId .. '/modify', DumpJson(modifyRequest))
-
-      -- upload the modified instance.  When performing modification at the instance level, all IDs from the original instance
-      -- are preserved (SOPInstanceUID, SeriesInstanceUID, StudyInstanceUID)
-      -- so when you'll upload it to Orthanc, it will overwrite the old instance only
-      -- if you've set the "OverwriteInstances" option to true in your configuration file
-      local uploadResponse = ParseJson(RestApiPost('/instances', modifiedDicom))
-
-      -- PrintRecursive(uploadResponse)
-      
-      if (uploadResponse["Status"] == 'AlreadyStored') then
-         print("Are you sure you've enabled 'OverwriteInstances' option ?")
-      end
-
-      if (uploadResponse["ID"] ~= instanceId) then
-         print("modified instance and original instance don't have the same Orthanc IDs !")
-      end 
-
-      print('replaced InstitutionName in instance ' .. instanceId)
-
-   end
-end
-
-function OnStableStudy(studyId, tags, metadata)
-   -- since we have modified tags that are stored in the Index DB at Study/Series level: InstitutionName and OperatorsName,
-   -- we need to reconstruct the Index DB data for this study
-
-   RestApiPost('/studies/' .. studyId .. '/reconstruct', "")
-
-   print('reconstructed Index DB data for study ' .. studyId)
-
-end
